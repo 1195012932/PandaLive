eef10558c9371ec884cd566e83f9b0804c1652e9
1195012932 <1195012932@qq.com>
2017-08-24 12:01:36 +0800

    第一次上传框架

--- app/src/main/java/com/example/panda/view/fragment/VideoFragment.java
+++ app/src/main/java/com/example/panda/view/fragment/VideoFragment.java
@@ -2,13 +2,12 @@ package com.example.panda.view.fragment;
 
 
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.chanven.lib.cptr.PtrClassicFrameLayout;
 import com.example.panda.R;
 import com.example.panda.base.BaseFragment;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,8 +16,8 @@ public class VideoFragment extends BaseFragment {
 
 
     private ImageView preson_sign;
-    private RecyclerView recycler;
-    private PtrClassicFrameLayout ptrClass;
+    private XRecyclerView recycler;
+
 
     @Override
     protected void loadData() {
@@ -39,13 +38,11 @@ public class VideoFragment extends BaseFragment {
     protected void initView(View view) {
         preson_sign = view.findViewById(R.id.preson_sign);
         recycler = view.findViewById(R.id.recycler);
-        ptrClass = view.findViewById(R.id.ptrClass);
+
     }
 
     @Override
     protected int getLayout() {
         return R.layout.fragment_video;
     }
-
-
 }
