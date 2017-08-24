00419b6add083be6a88fadf489598da083f408bb
1195012932 <1195012932@qq.com>
2017-08-24 21:59:12 +0800

    李鑫修改了Video模块

--- app/src/main/java/com/example/panda/adapters/MyAdapter.java
+++ app/src/main/java/com/example/panda/adapters/MyAdapter.java
@@ -1,100 +1,68 @@
 package com.example.panda.adapters;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.panda.R;
-import com.example.panda.model.entity.Bean;
-import com.youth.banner.Banner;
+import com.example.panda.model.entity.VideoBean;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+public class MyAdapter extends BaseAdapter {
     private Context context;
-    ArrayList<Bean> list = new ArrayList<>();
-    private Listener listener;
-    private ViewHolder holders;
+    private List<VideoBean.ListBean> listBeen;
+    private ViewHolder holder;
 
-    public MyAdapter(Context context, ArrayList<Bean> list) {
+    public MyAdapter(Context context, List<VideoBean.ListBean> listBeen) {
         this.context = context;
-        this.list = list;
+        this.listBeen = listBeen;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public int getCount() {
+        return listBeen.size();
+    }
 
-        if (viewType == 1) {
-            View view1 = View.inflate(context, R.layout.video_item1, null);
-            holders = new ViewHolder(view1);
+    @Override
+    public Object getItem(int position) {
+        return listBeen.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = View.inflate(context, R.layout.video_item2, null);
+            holder.title = (TextView) convertView.findViewById(R.id.video_name);
+            holder.info = (TextView) convertView.findViewById(R.id.video_info);
+            holder.time = (TextView) convertView.findViewById(R.id.video_time);
+            holder.icon = (ImageView) convertView.findViewById(R.id.video_icon);
+            convertView.setTag(holder);
         } else {
-            View view2 = View.inflate(context, R.layout.video_item1, null);
-            holders = new ViewHolder(view2);
+            holder = (ViewHolder) convertView.getTag();
         }
-
-
-        return holders;
+        VideoBean.ListBean bean = listBeen.get(position);
+        holder.title.setText(bean.getTitle());
+        holder.info.setText(bean.getBrief());
+        holder.time.setText(bean.getVideoLength());
+        Glide.with(context).load(bean.getImage()).error(R.mipmap.panda_sign).into(holder.icon);
+        return convertView;
     }
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-
-       /* if (holder.image1 != null) {
-            holder.image1.setImageResource(list.get(position).getImg());
-        }
-        if (holder.image2_1 != null) {
-            holder.image2_1.setImageResource(list.get(position).getImg());
-        }
-        if (holder.text1 != null) {
-            holder.text1.setText(list.get(position).getName());
-        }
-        if (holder.image2_2 != null) {
-            holder.image2_2.setImageResource(list.get(position).getImg());
-        }
-        if (holder.text2 != null) {
-            holder.text2.setText(list.get(position).getName());
-        }
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listener.click(v, position);
-            }
-        });*/
-    }
-
-    @Override
-    public int getItemCount() {
-        return list.size();
-    }
-
-    public int getItemViewType(int position) {
-
-       /* int k = list.get(position).getFlag();
-        if (k == 1) {
-            return 1;
-        } else {
-            return 2;
-        }*/
-        return 0;
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-
-        private Banner image1;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            image1 = (Banner) itemView.findViewById(R.id.banner);
-
-        }
-    }
-
-    public void setOnItemClickListener(Listener listener) {
-        this.listener = listener;
-    }
-
-    interface Listener {
-        void click(View v, int position);
+    static class ViewHolder {
+        TextView title;
+        TextView info;
+        TextView time;
+        ImageView icon;
     }
 }
