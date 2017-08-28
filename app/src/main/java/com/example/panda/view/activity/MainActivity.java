7739b76e241c3a5f66a81e16afd03626a891d1aa
1195012932 <1195012932@qq.com>
2017-08-28 21:50:24 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/example/panda/view/activity/MainActivity.java
+++ app/src/main/java/com/example/panda/view/activity/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.panda.view.activity;
 
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -9,6 +15,7 @@ import android.widget.Toast;
 import com.example.panda.R;
 import com.example.panda.base.BaseActivity;
 import com.example.panda.utils.FragmentBuilder;
+import com.example.panda.utils.Netwoke;
 import com.example.panda.view.fragment.BroadcastFragment;
 import com.example.panda.view.fragment.ChinaFragment;
 import com.example.panda.view.fragment.HomeFragment;
@@ -23,6 +30,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private RadioButton rb_video;
     private RadioButton rb_broad;
     private RadioButton rb_china;
+    private Netwoke netwoke;
 
     //方清正
     @Override
@@ -62,7 +70,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void initData() {
-
+        getnetwoke();
     }
 
     @Override
@@ -73,7 +81,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         rb_video = (RadioButton) findViewById(R.id.rb_video);
         rb_broad = (RadioButton) findViewById(R.id.rb_broad);
         rb_china = (RadioButton) findViewById(R.id.rb_china);
-
     }
 
     @Override
@@ -115,5 +122,88 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+    //判断网络状态
+    private void getnetwoke() {
+
+        if (netwoke == null) {
+            netwoke = new Netwoke();
+        }
+
+        String getnetwoke = netwoke.getnetwoke(MainActivity.this);
+
+        Toast.makeText(MainActivity.this, getnetwoke, Toast.LENGTH_SHORT).show();
+
+        if (getnetwoke.equals("网络无连接")) {
+            setNetwork();
+        }
+    }
+
+    private void setNetwork() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setIcon(R.mipmap.ic_launcher);
+
+        builder.setTitle("无法连接网络");
+
+        builder.setMessage("网络不可用，如果继续，请先设置网络！");
+
+        builder.setPositiveButton("设置", new DialogInterface.OnClickListener() {
+
+            @Override
+
+            public void onClick(DialogInterface dialog, int which) {
+
+                Intent intent = null;
+
+                /**
+
+                 * 判断手机系统的版本！如果API大于10 就是3.0+
+
+                 * 因为3.0以上的版本的设置和3.0以下的设置不一样，调用的方法不同
+
+                 */
+
+                if (Build.VERSION.SDK_INT > 10) {
+
+                    intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+
+                } else {
+
+                    intent = new Intent();
+
+                    ComponentName component = new ComponentName(
+
+                            "com.android.settings",
+
+                            "com.android.settings.WirelessSettings");
+
+                    intent.setComponent(component);
+
+                    intent.setAction("android.intent.action.VIEW");
+
+                }
+
+                startActivity(intent);
+
+            }
+
+        });
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+            @Override
+
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+
+        });
+
+        builder.create();
+
+        builder.show();
     }
 }
